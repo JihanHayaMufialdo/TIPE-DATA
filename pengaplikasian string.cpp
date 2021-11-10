827677455fa41a210a032697811f30e6e6b787ff
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-10 22:40:31 +0700

    Add files via upload

---
+++ pengaplikasian string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string jurusan = "Ilmu Komputer";
+  string kampus  = "Universitas Lampung";
+  string nohp    = "082345678912";
+ 
+  cout << jurusan << endl;
+  cout << kampus << endl;
+  cout << nohp << endl;
+ 
+  return 0;
+}
