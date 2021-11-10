8267ec6c7ea7bb3f7604ef7a09fafaaa11bbc55f
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-10 22:38:53 +0700

    Add files via upload

---
+++ pengaplikasian char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char foo = 67;
+  char bar = 'Z';
+ 
+  cout << "Isi variabel foo: " << foo << endl;
+  cout << "Isi variabel bar: " << bar << endl;
+ 
+  return 0;
+}
