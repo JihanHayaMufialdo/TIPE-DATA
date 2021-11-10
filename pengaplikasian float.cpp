891a972ca037c1795bffb16c7d0af99283184bce
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-10 22:39:56 +0700

    Add files via upload

---
+++ pengaplikasian float.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float  des1;
+  float  des2;
+ 
+  des1 = 3.957;
+  des2 = 0.019;
+ 
+  cout << "Isi des1 = "  << des1 << endl;
+  cout << "Isi des2 = "  << des2 << endl;
+ 
+  return 0;
+}
